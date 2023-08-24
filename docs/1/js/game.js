97fccb40186dc725bb046ed2e96e2e1b5208c25f
ytyaru <yttry0@gmail.com>
2023-08-24 10:56:53 +0900

    1 Game（最低限の流れ）

--- docs/1/js/game.js
+++ docs/1/js/game.js
@@ -22,6 +22,9 @@ class Game {
         this.rule.thinkSec = [10, 5, 3]
         this.deck = new Deck(this.rule.deck.count, this.rule.deck.withJoker)
         this.players = [new Player('A'), new Player('B'), new Player('C'), new Player('D')]
+        this.stages = []
+        this.values = []
+        this.points = []
     }
     #show() { // rule, player, deck 表示
         console.log(this.rule)
@@ -46,14 +49,16 @@ class Game {
             console.log(`${r+1} Round`)
             // thinkSec秒だけキー入力待受する
             // 手
-            const stage = this.players.map(p=>p.pullOut())
+            //const stage = this.players.map(p=>p.pullOut())
+            const stage = this.players.map(p=>new Card(p.pullOut()))
             console.log(stage)
             this.stages.push(stage)
             this.#calc(stage)
             console.log(stage)
-            this.#judge(stage)
+            this.#judge(r, this.values[r])
             //console.log(this.stages[r].map(s=>Card.fromId(s).Char))
-            console.log(stage.map(s=>Card.fromId(s)))
+            //console.log(stage.map(s=>Card.fromId(s)))
+            console.log(stage.map(c=>c.Char))
             console.log(this.values[r])
             console.log(this.points[r])
         }
@@ -61,20 +66,51 @@ class Game {
     #calc(stage) { // 点数計算
         const values = []
         for (let s=0; s<stage.length; s++) {
-            const num = Card.fromId(stage[s]).Num
+            //const card = new Card(stage[s])
+            //const num = Card.fromId(stage[s]).Num
+            //console.log(card)
+            console.log(stage[s])
             // Round rule によって条件判定＆増減
-            const value = num
-            this.values.push(value)
+            //const value = card.Num
+            const value = stage[s].Num
+            values.push(value)
         }
+        this.values.push(values)
         return values
     }
+    #judge(r) {
+        this.points.push([0,0,0,0])
+        const values = this.values[r]
+        const maxIdx = values.indexOf(Math.max(...values));
+        console.log(values, maxIdx, Math.max(...values))
+        // rule によって判定＆増減
+        if (1===values.filter(v=>v===values[maxIdx]).length) { // win/lose
+            this.points[r][maxIdx] = 1
+        } else { } // draw
+    }
+    /*
     #judge(stage) {
         const maxIdx = stage.indexOf(Math.max(stage));
         if (1===stage.some(v=>v===maxIdx).length) { // win/lose
             this.points[maxIdx] = 1
         } else { } // draw
     }
+    */
     #result() {
+        console.log(`Result`)
+        const winpoints = []
+        for (let p=0; p<this.players.length; p++) { winpoints.push(0) }
+        for (let p=0; p<this.players.length; p++) {
+            for (let r=0; r<this.rule.round; r++) { winpoints[p] += this.points[r][p] }
+        }
+        console.log(winpoints)
+
+        const maxIdx = winpoints.indexOf(Math.max(...winpoints));
+        console.log(winpoints, maxIdx, Math.max(...winpoints))
+        // rule によって判定＆増減
+        if (1===winpoints.filter(v=>v===winpoints[maxIdx]).length) { // win/lose
+            console.log(`Winner [${maxIdx}] ${this.players[maxIdx].Name}`)
+        } else { console.log(`Draw`) } // draw
 
     }
 }
--- docs/1/js/player.js
+++ docs/1/js/player.js
@@ -7,5 +7,6 @@ class Player {
     get Name() { return this.name }
     get Hands() { return this.hands }
     draw(id) { this.hands.push(id); this.hands.sort(); }
-    pullOut() { return this.hands[0] } // 戦略アルゴリズムによって出す手を決定する
+    //pullOut() { return this.hands[0] } // 戦略アルゴリズムによって出す手を決定する
+    pullOut() { return this.hands.shift() } // 戦略アルゴリズムによって出す手を決定する
 }
